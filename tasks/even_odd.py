cd0a4f4d9880851480d2178d6613591cdbf1f583
Ekaterina Petrenko <katch.petrenko@gmail.com>
2023-02-12 19:42:33 +0300

    corrections

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -6,5 +6,7 @@ __all__ = (
 def even_odd(arr: list[int]) -> float:
     even = [i for i in arr if i%2 == 0]
     odd = [i for i in arr if i%2 != 0]
-    return sum(even)/sum(odd)    
-    
+    if len(even) and len(odd) > 0:
+        return sum(even)/sum(odd)    
+    else:
+        return 0
--- tasks/text.py
+++ tasks/text.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import re
 
 __all__ = (
     'find_shortest_longest_word',
@@ -10,24 +11,29 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    text = re.sub('\s', ' ', text)
-    txt = text.split()
-    shortest = [txt[0]]
-    longest = [txt[0]]
-    for i in txt[1:]:
-        if len(i) < len(shortest[0]):
-            shortest[0] = i
-        elif len(i) == len(shortest[0]):
-            shortest.append(i)
-        elif len(i) > len(longest[0]):  
-            longest[0] = i
-        elif len(i) == len(longest[0]):  
-            longest.append(i)
-    if len(shortest) == 1 and len(longest) == 1:
-        return shortest[0], longest[0]
-    elif len(shortest) == 1 and len(longest) != 1:
-        return shortest[0], None
-    elif len(shortest) != 1 and len(longest) == 1:
-        return None, longest[0]
-    else:
+    if re.fullmatch('\s+', text):
         return None, None
+    else:
+        text = re.sub('\s+', ' ', text)
+        if len(text) > 0:
+            txt = text.split()
+            shortest = [txt[0]]
+            longest = [txt[0]]
+            for i in txt[1:]:
+                if len(i) < len(shortest[0]):
+                    shortest[0] = i
+                elif len(i) == len(shortest[0]):
+                    shortest.append(i)
+                elif len(i) > len(longest[0]):  
+                    longest[0] = i
+                elif len(i) == len(longest[0]):  
+                    longest.append(i)
+            if len(shortest) == 1 and len(longest) == 1:
+                return shortest[0], longest[0]
+            elif len(shortest) == 1 and len(longest) != 1:
+                return shortest[0], None
+            elif len(shortest) != 1 and len(longest) == 1:
+                return None, longest[0]
+        else:
+            return None, None
+print(find_shortest_longest_word(''))

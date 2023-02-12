0e5a5e3da737d796b8c67d3febf48cf601a08815
Ekaterina Petrenko <katch.petrenko@gmail.com>
2023-02-12 13:35:35 +0300

    homework1

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -4,9 +4,7 @@ __all__ = (
 
 
 def even_odd(arr: list[int]) -> float:
-    """
-    Функция возвращает отношение суммы четных элементов массив к сумме нечетных
-    Пример:
-    even_odd([1, 2, 3, 4, 5]) == 0.8889
-    """
-    raise NotImplementedError
+    even = [i for i in arr if i%2 == 0]
+    odd = [i for i in arr if i%2 != 0]
+    return sum(even)/sum(odd)    
+    

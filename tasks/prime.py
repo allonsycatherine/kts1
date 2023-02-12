a3914e7131ee3c75e609bd00e118b8ab4a86e7a8
Ekaterina Petrenko <katch.petrenko@gmail.com>
2023-02-12 13:49:32 +0300

    prime update

--- tasks/prime.py
+++ tasks/prime.py
@@ -8,8 +8,9 @@ def is_prime(number: int) -> bool:
     """
     Функция должна вернуть True если число является простым, иначе - False
     """
-    if (fac(number - 1)+1)%number == 0:
-        return True
+    if number > 1:
+        if (fac(number - 1)+1)%number == 0:
+            return True
     else:
         return False
 
